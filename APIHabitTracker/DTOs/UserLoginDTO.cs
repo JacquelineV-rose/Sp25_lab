01c716ca3e3c9291ea1197c602c1408ff35b8cfe
megcoxx <mcox6232@gmail.com>
2025-03-30 22:58:52 -0400

    Fixed user register page to include email

--- APIHabitTracker/DTOs/UserLoginDTO.cs
+++ APIHabitTracker/DTOs/UserLoginDTO.cs
@@ -1,6 +1,6 @@
 namespace HabitTrackerAPI.DTOs
 {
-    public class LoginDto
+    public class UserLoginDto
     {
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
